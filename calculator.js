a3d5556999a2fcfe0cb6653cb0ed42b0cea24548
ta121443 <ryo_uchi4_20@outlook.jp>
2022-05-04 00:47:43 +0900

    version 1.2

--- calculator.js
+++ calculator.js
@@ -7,10 +7,54 @@ let nowText = "";     //演算子の後の数
 let operator = "";      //何の演算子が入力されたのか
 let afterFirstFlag = true;
 let answer = 0;
+let equalFlag = false;
+let clearFlag = false;
+
+const allClear = () => {
+    beforeText = "";
+    nowText = "";
+    operator = "";
+    afterFirstFlag = true;
+    answer = 0;
+    equalFlag = false;
+    clearFlag = false;
+}
+
+const clear = () => {
+    nowText = "";
+    afterFirstFlag = true;
+    answer = 0;
+    clearFlag = true;    
+}
+
+const calculation = (operator) => {
+    if(operator === "+") {
+        answer = Number(beforeText) + Number(nowText);
+    } else if(operator === "-") {
+        answer = Number(beforeText) - Number(nowText);
+    } else if(operator === "*") {
+        answer = Number(beforeText) * Number(nowText);
+    } else if(operator === "/") {
+        answer = Number(beforeText) / Number(nowText);
+    }
+
+    if((operator === "/") && (answer.toString().length > 9)) {
+        return answer.toPrecision(9);
+    } else {
+        return String(answer);
+    }
+}
 
 const buttonPressed = (event) => {
     const text = event.target.textContent;
-    if(beforeText.length < 10 && nowText.length < 10){    //画面の幅の都合上
+
+    if(text === "C") {
+        if(clearFlag || equalFlag) {     //ACシステム
+            allClear();
+        } else {
+            clear();
+       }
+    } else if(beforeText.length < 10 && nowText.length < 10){    //画面の幅の都合上
 
         //%%%%%%%%%%%%%演算子が入力される前%%%%%%%%
         if(operator.length === 0){      
@@ -24,6 +68,11 @@ const buttonPressed = (event) => {
                 nowText += text;
             }
         } else {    //%%%%%%%%演算子が入力された後%%%%%%%%%
+            if(isNaN(Number(text))) {
+                if(!(text === "=")) {
+                    operator = text;
+                }
+            }
             if(!(isNaN(Number(text)))) {
                 if(afterFirstFlag) {        //最初の数字が入力されるまでは数字を表示したままにしておく
                     nowText = text;
@@ -33,24 +82,10 @@ const buttonPressed = (event) => {
                 }
             }
             if (text === "=") {
-                if(operator === "+") {
-                    answer = Number(beforeText) + Number(nowText);
-                    nowText = String(answer);
-                } else if(operator === "-") {
-                    answer = Number(beforeText) - Number(nowText);
-                    nowText = String(answer);
-                } else if(operator === "*") {
-                    answer = Number(beforeText) * Number(nowText);
-                    nowText = String(answer);
-                } else if(operator === "/") {
-                    answer = Number(beforeText) / Number(nowText);
-                    nowText = String(answer);
-                }
+                equalFlag = true;
+                nowText = calculation(operator);
             } 
         }
-    }
-    if(text === "C") {
-        nowText = "";
     }
     $result.textContent = nowText;
 }
